42807b45a11596a72872b584852be92b89f84ef2
Minh <minhdotcom@gmail.com>
2019-04-16 10:59:59 +0700

    done

--- functions.js
+++ functions.js
@@ -1,17 +1,54 @@
+var knight = "<img src = 'img/knight.png' width='40'></img>";
+var knightStep = "<img src = 'img/knightStep.png' width='40'></img>";
+var black = "<img src = 'img/blackSquare.png' width='40'></img>";
+var white = "<img src = 'img/whiteSquare.jpg' width='40'></img>";
+var gameField = document.getElementById("gameField");
+var board = "<table width = '300' cellspacing = '0' cellpadding='0' border = '0'>";
+var isSquare = true;
+var square;
+var knightX;
+var knightY;
+var knightSteps = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]
+];
+
+function turn_square_black_white () {
+    isSquare = !isSquare;
+    if (isSquare) {
+        square = black;
+    } else {
+        square = white;
+    }
+}
+
 function draw_board () {
-    for (i = 1; i <= 8; i++) {
-        board += "<tr>";
+    board += "<tr>" +
+        "<td></td>" +
+        "<td>&nbsp&nbsp&nbsp&nbsp1</td>" +
+        "<td>&nbsp&nbsp&nbsp&nbsp2</td>" +
+        "<td>&nbsp&nbsp&nbsp&nbsp3</td>" +
+        "<td>&nbsp&nbsp&nbsp&nbsp4</td>" +
+        "<td>&nbsp&nbsp&nbsp&nbsp5</td>" +
+        "<td>&nbsp&nbsp&nbsp&nbsp6</td>" +
+        "<td>&nbsp&nbsp&nbsp&nbsp7</td>" +
+        "<td>&nbsp&nbsp&nbsp&nbsp8</td>" +
+        "</tr>"
+    for (i = 0; i < 8; i++) {
+        board += "<tr>" + "<td>" + (i + 1) + "&nbsp&nbsp&nbsp" + "</td>";
         isSquare = !isSquare;
-        for (j = 1; j <= 8; j++) {
-            isSquare = !isSquare;
-            if (isSquare) {
-                square = black;
-            } else {
-                square = white;
-            }
+        for (j = 0; j < 8; j++) {
+
+            turn_square_black_white()
             if (j == knightX && i == knightY) {
                 board += "<td>" + knight + "</td>";
-            } else if (j == knightStepX && i == knightStepY) {
+            } else if (knightSteps[j][i] == 1) {
                 board += "<td>" + knightStep + "</td>";
             } else {
                 board += "<td>" + square + "</td>";
@@ -19,14 +56,14 @@ function draw_board () {
         }
         board += "</tr>";
     }
-    board += "</table><br/><br/><button onclick='set_knight()'>Move knight</button>";
+    board += "</table><br/><br/><button onclick='set_knight()'>Set knight</button><br/><br/>";
     gameField.innerHTML = board;
 }
 
 function set_knight () {
-    knightX = Number(prompt("Please input X for Knight."));
-    knightY = Number(prompt("Please input Y for Knight."));
+    knightX = Number(prompt("Please input X for Knight.")) - 1;
+    knightY = Number(prompt("Please input Y for Knight.")) - 1;
+    board = "<table width = '300' cellspacing = '0' cellpadding='0' border = '0'>";
     draw_board();
-    knightStepX = knightX;
-    knightStepY = knightY;
+    knightSteps[knightX][knightY] = 1;
 }
